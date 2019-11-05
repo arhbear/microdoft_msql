f9f348bafd735033fc29e9cc0f7819c56d2bb57b
arhbear <56405667+arhbear@users.noreply.github.com>
2019-11-05 14:59:59 +0300

    Update agregate_and_group_funct.sql

--- agregate_and_group_funct.sql
+++ agregate_and_group_funct.sql
@@ -3,14 +3,14 @@ use EDUCATION
 select count(STUDENT_ID) from EXAM_MARKS where SUBJ_ID = 22
 select count(distinct(SUBJ_ID)) from EXAM_MARKS
 select STUDENT_ID,max(MARK) from EXAM_MARKS group by STUDENT_ID
-select min(surname) from STUDENT where SURNAME like 'È%'
+select min(surname) from STUDENT where SURNAME like 'Ãˆ%'
 select SUBJ_NAME, max(semestr) as semestr from SUBJECT group by SUBJ_NAME
 select EXAM_DATE, count(STUDENT_ID) as st_count from EXAM_MARKS group by EXAM_DATE
 select AVG(MARK) as average_mark, SUBJ_ID from EXAM_MARKS group by SUBJ_ID
 select AVG(MARK) as average_mark, STUDENT_ID from EXAM_MARKS group by STUDENT_ID
 select AVG(MARK) as average_mark, EXAM_ID from EXAM_MARKS group by EXAM_ID
 select count(STUDENT_ID) as st_c, EXAM_ID from EXAM_MARKS where mark is not null group by EXAM_ID 
-select count(SUBJ_ID) as subj_c, semestr from SUBJECT group by SEMESTR
+select count(SUBJ_ID) as subj_c, ((SEMESTR + 1) / 2) as KURS_NUMBER from SUBJECT group by ((SEMESTR + 1) / 2)
 select STUDENT_ID, SURNAME, STIPEND*1.2 as stipend from STUDENT order by stipend
 select STUDENT_ID, SURNAME, STIPEND*1.2 as stipend from STUDENT order by SURNAME
 select STUDENT_ID, max(mark) as max_mark, min(mark) as min_mark from EXAM_MARKS group by STUDENT_ID
